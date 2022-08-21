6f88ac27e7de3a01c4fe9aa8afcbc7875f98ec38
heph3astus <kiernanolson@gmail.com>
2022-08-20 20:18:16 -0400

    About page v1, fixed syntax highlighting capslock issue

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ import {
 import { useMediaQuery } from 'react-responsive';
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate
+} from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { AiOutlineBorderOuter } from "react-icons/ai";
+import About from "./components/about"
 
 function App() {
 
@@ -22,39 +32,54 @@ function App() {
     undefined,
     (isSystemDark) => setTheme(isSystemDark)
   );
+  const [cookies, setCookie] = useCookies(['pageState'])
 
   return (
-    <div className={`flex h-screen ${theme}`}>
-      <ToastContainer
-                        position="bottom-center"
-                        autoClose={5000}
-                        hideProgressBar={false}
-                        newestOnTop={false}
-                        closeOnClick
-                        rtl={false}
-                        pauseOnFocusLoss
-                        draggable
-                        pauseOnHover
-                        theme={(theme) ? "dark" : "light"}
-      />
-      <FileManager
-        code={code}
-        setCode={setCode}
-        setAwaitingUpload={setAwaitingUpload}
-        theme={theme}
-        setFileName={setFileName}
-      />
-      <Editor 
-        code={code}
-        onChange={setCode}
-        fileName={fileName}
-        loading={awaitingUpload}
-        theme={theme}
-      />
-      <Sidebar 
-        code={code}
-      />
-    </div>
+    <Router>
+    <Routes>
+      <Route path="/editor" element={
+        <div className={`flex h-screen ${theme}`}>
+          <ToastContainer
+                            position="bottom-center"
+                            autoClose={5000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover
+                            theme={(theme) ? "dark" : "light"}
+          />
+          <FileManager
+            code={code}
+            setCode={setCode}
+            setAwaitingUpload={setAwaitingUpload}
+            theme={theme}
+            setFileName={setFileName}
+          />
+          <Editor
+            code={code}
+            onChange={setCode}
+            fileName={fileName}
+            loading={awaitingUpload}
+            theme={theme}
+          />
+          <Sidebar
+            code={code}
+          />
+        </div>
+      } />
+      <Route path="/about" element={
+        <About setCookie={setCookie} theme={theme} />
+      } />
+      <Route path="/" element={
+        <>
+          {(cookies.pageState === true) ? <Navigate to="/editor" /> : <Navigate to="/about" />}
+        </>
+      } />
+    </Routes>
+    </Router>
   );
 }
 
